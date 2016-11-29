95718f7490da8e3b80f5bbe1f4230ed199954701
LeeMinglu <402007353@qq.com>
2016-11-29 13:51:38 +0800

    将项目所需的资料准备完毕。

---
+++ QQChat_Swift3.0_1/QQChat_Swift3.0_1/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QQChat_Swift3.0_1
+//
+//  Created by 李明禄 on 2016/11/29.
+//  Copyright © 2016年 SocererGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
